30a338e4442583bb11427db4d312807031e26f53
RemiPatay <71498506+RemiPatay@users.noreply.github.com>
2021-12-30 16:37:31 +0100

    Update marketplace.js

--- cw721/marketplace.js
+++ cw721/marketplace.js
@@ -8,4 +8,9 @@ module.exports = [
     "link": "https://knowhere.art",
     "icon": "https://knowhere.art/static/logos/knowhere-logo.svg"
   },
+  {
+    "name": "Talis",
+    "link": "https://talis.art",
+    "icon": "https://talis.art/icons/talis/logo.svg"
+  },
 ];
